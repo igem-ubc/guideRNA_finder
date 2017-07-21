de052c51bf69b856691ddf7ddf9e37b20dfd20a9
Leif Pedersen <leifcnp@gmail.com>
2017-07-20 19:18:09 -0700

    added gitignore

--- Cas9_Calculator/testCas9Calculator.py
+++ Cas9_Calculator/testCas9Calculator.py
@@ -2,7 +2,7 @@ from Cas9_Calculator import *
 
 if __name__ == "__main__":
 
-    guideSequence = 'TACGTACACAAGAGCTCTAG'   
+    guideSequence = 'TACGTACACAAGAGCTCTAG'
     Cas9Calculator = clCas9Calculator(['/home/connor/Bioinformatics/iGEM/NC_000913.gb'])
     sgRNA1 = sgRNA(guideSequence, Cas9Calculator)
     sgRNA1.run()
